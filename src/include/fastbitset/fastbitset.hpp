b8c8dc7b46261422f0a3f24635a6b9151327f84f
Notiooo <dawidgrobert@gmail.com>
2025-03-16 21:05:08 +0100

    Use upper-case naming for static constants
    I decided to use upper-case naming for static constants
    to explicitly mark they're compile time.

--- src/include/fastbitset/fastbitset.hpp
+++ src/include/fastbitset/fastbitset.hpp
@@ -24,8 +24,8 @@ class FastBitset
     static_assert(CONTAINER_SIZE != 0, "Container can't be of size 0.");
     static_assert(std::is_unsigned_v<CHUNK_TYPE>, "CHUNK_TYPE must be an unsigned integer");
     static_assert(std::is_unsigned_v<CHUNK_TYPE>, "CHUNK_TYPE must be an unsigned integer");
-    static constexpr auto bitsPerChunk = std::numeric_limits<CHUNK_TYPE>::digits;
-    static constexpr auto numberOfChunks = (CONTAINER_SIZE + bitsPerChunk - 1) / bitsPerChunk;
+    static constexpr auto BITS_PER_CHUNK = std::numeric_limits<CHUNK_TYPE>::digits;
+    static constexpr auto NUMBER_OF_CHUNKS = (CONTAINER_SIZE + BITS_PER_CHUNK - 1) / BITS_PER_CHUNK;
 
 
 public:
@@ -74,16 +74,16 @@ private:
     template<std::unsigned_integral T>
     constexpr void storeValue(T value)
     {
-        for (std::size_t i = 0; i < numberOfChunks and value != 0; ++i)
+        for (std::size_t i = 0; i < NUMBER_OF_CHUNKS and value != 0; ++i)
         {
             mData[i] = static_cast<CHUNK_TYPE>(value);
-            value >>= bitsPerChunk;
+            value >>= BITS_PER_CHUNK;
         }
     }
 
     constexpr void truncateExcessBits() noexcept
     {
-        constexpr auto remainderBits = CONTAINER_SIZE % bitsPerChunk;
+        constexpr auto remainderBits = CONTAINER_SIZE % BITS_PER_CHUNK;
 
         if constexpr (remainderBits != 0)
         {
@@ -103,7 +103,7 @@ private:
     }
 
 private:
-    std::array<CHUNK_TYPE, numberOfChunks> mData{};
+    std::array<CHUNK_TYPE, NUMBER_OF_CHUNKS> mData{};
 };
 
 }// namespace fastbitset
